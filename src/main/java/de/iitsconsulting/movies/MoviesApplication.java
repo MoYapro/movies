a2e1e92f6ff124736be74932936fff816681a4da
Ilia Barancic <ilia.barancic@iits-consulting.de>
2021-06-10 07:58:12 +0200

    wip 3

--- src/main/java/de/iitsconsulting/movies/MoviesApplication.java
+++ src/main/java/de/iitsconsulting/movies/MoviesApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"de.iitsconsulting.movies.repo", "de.iitsconsulting.movies.controller"})
 public class MoviesApplication {
 
     public static void main(String[] args) {
--- src/main/java/de/iitsconsulting/movies/repo/MyMovieRepositoryImplementationImpl.java
+++ src/main/java/de/iitsconsulting/movies/repo/MyMovieRepositoryImplementationImpl.java
@@ -24,9 +24,10 @@ public class MyMovieRepositoryImplementationImpl {
     }
 
     public List<Movie> findMovieByYear(int startYear, int endYear) {
-        Query query = entityManager.createQuery("SELECT m FROM Movie m where m.year between :startYear and 0");
-        query.setParameter("startYear", startYear);
-        return query.getResultList();
+        if (startYear > endYear ) {
+            return entityManager.createQuery("SELECT m FROM Movie m WHERE m.year >=" + startYear).getResultList();
+        }
+        return entityManager.createNativeQuery("SELECT * FROM movie", Movie.class).getResultList();
     }
 
     public <S extends Movie> S save(final S s) {return repository.save(s);}
@@ -37,7 +38,7 @@ public class MyMovieRepositoryImplementationImpl {
 
     public MyMovieRepositoryImplementationImpl(final MovieMovieRepository repository) {this.repository = repository;}
 
-    public  Optional<Movie> findById(long id) { return repository.findById(id);}
+    public Optional<Movie> findById(long id) { return repository.findById(id);}
 
     public List<Movie> findAll() { return repository.findAll();}
 
